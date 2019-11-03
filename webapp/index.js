d90f0196ff7d24c4aae1e2ef99816b10e0aef105
Will Ye <wy35@duke.edu>
2019-11-03 01:24:51 -0500

    frontend works

--- webapp/index.js
+++ webapp/index.js
@@ -1,25 +1,23 @@
 const express = require('express')
 const ws = require('ws')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT || '8000'
 
-app.set('view engine', 'pug')
-app.set('views', './views')
+app.use(express.static('public'))
 
 const server = new ws.Server({ server: app.listen(port) })
 
 server.on('connection', socket => {
     socket.on('message', msg => {
-        /*
         server.clients.forEach(client => {
             client.send(msg);
         })
-        */
         console.log(msg)
     })
 })
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.sendFile(path.join(__dirname + '/index.html'))
 })
